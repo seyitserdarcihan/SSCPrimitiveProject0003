48481f9f912781828b94f6249e6ccdb380938551
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-12 19:25:00 +0300

    Learning Java 2023-09-12-001

---
+++ src/learning001/IsWorldHell.java
@@ -0,0 +1,11 @@
+package learning001;
+
+public class IsWorldHell {
+
+    public static void main(String[] args) {
+
+        System.out.println("There is lava, but the world is not Hell");
+        System.out.println("Because, the world is mixed with happiness and misery");
+        System.out.println("But in Hell, there is only misery and misery");
+    }
+}
