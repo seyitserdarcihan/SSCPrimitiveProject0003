fe3e14d0fda8eafe4080c3ed0876d7a98c4e6b08
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-16 16:54:45 +0300

    slowly and slowly learning Java, very slowly indeed

---
+++ src/learning004/Strings01.java
@@ -0,0 +1,16 @@
+package learning004;
+
+public class Strings01 {
+
+    String nombre = "Humphrey Carpenter";   //Bir dizgiye değer atarken, çift tırnak kullanılır
+                                           //Örnekte, Humphrey Carpenter ismini kapsayan çift tırnak.
+
+    nombre.
+}
+
+
+/* nombre isimli değişkenin yanına nokta ekledim ama IntelliJ IDEA
+   metot önerisi yapan bir menü açmıyor. "Identifier expected:8" uyarısı veriyor.
+   Neyi yanlış yapıyorum?
+ */
+
