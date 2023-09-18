fa765bfa97accc8bf0f70edfa670da346cd7b451
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-18 21:50:26 +0300

    Learning Java 2023-09-18-001

--- src/learning004/Strings01.java
+++ src/learning004/Strings01.java
@@ -2,15 +2,24 @@ package learning004;
 
 public class Strings01 {
 
-    String nombre = "Humphrey Carpenter";   //Bir dizgiye değer atarken, çift tırnak kullanılır
-                                           //Örnekte, Humphrey Carpenter ismini kapsayan çift tırnak.
 
-    nombre.
+
+    public static void main(String[] args) {
+
+        String nombre = "Humphrey Carpenter";   //Bir dizgiye değer atarken, çift tırnak kullanılır
+                                                //Örnekte, Humphrey Carpenter ismini kapsayan çift tırnak.
+
+        System.out.println(nombre);
+
+    }
+
+
 }
 
 
-/* nombre isimli değişkenin yanına nokta ekledim ama IntelliJ IDEA
-   metot önerisi yapan bir menü açmıyor. "Identifier expected:8" uyarısı veriyor.
-   Neyi yanlış yapıyorum?
+/* nombre isimli değişkenin hemen sağına bir nokta koyduğunda, IntelliJ IDEA,
+   metot önerileri listeleyen bir menü açar.
+   metotun adının harflerini yazdıkça, öneri menüsündeki listede gösterilenler
+   azalır.
  */
 
