770fa79f43c65ce6d8eec739d5098e22cf494cde
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-21 18:35:14 +0300

    bakalım uzun git push --set-upstream origin komutunu kullanmak gerekiyor mu

--- src/learning008/studyingMerge01.java
+++ src/learning008/studyingMerge01.java
@@ -5,5 +5,8 @@ public class studyingMerge01 {
     public static void main(String[] args) {
 
         System.out.println("merge çalışmaya devam ediyorum");
+        System.out.println("bakalım bir sonraki push işlemlerinde\n" +
+                "şu komutu kullanmak gerekiyor muymuş:\n" +
+                "git push --set-upstream origin <dal ismi>");
     }
 }
