e4374c3ad3eafe2e1fe39b0ebf7a4ea250d9c186
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-21 16:26:44 +0300

    çakışmalar çözüldükten sonra

--- src/learning005/learningBranches01.java
+++ src/learning005/learningBranches01.java
@@ -5,8 +5,7 @@ public class learningBranches01 {
     public static void main(String[] args) {
 
         System.out.println("Kendi dalımda çalışmayı öğreniyorum");
-
-        System.out.println("satırı değiştirip çakışma yapalım");
+        System.out.println("GitHub çakışmaları sanırım not etmiyor, hiç olmamışlar gibi");
 
     }
 }
