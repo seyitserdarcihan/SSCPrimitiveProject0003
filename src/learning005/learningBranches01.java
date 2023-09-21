4668a0732b38b2e396491189b2d686308279577b
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-21 14:09:30 +0300

    ilk defa ana dal dışındaki bir dalı komit ediyorum

---
+++ src/learning005/learningBranches01.java
@@ -0,0 +1,10 @@
+package learning005;
+
+public class learningBranches01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Kendi dalımda çalışmayı öğreniyorum");
+
+    }
+}
