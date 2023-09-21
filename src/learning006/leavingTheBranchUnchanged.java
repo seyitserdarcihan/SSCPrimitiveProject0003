6d65fae1f4aadd2b343299e2c167a7d4392878b3
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-21 15:25:06 +0300

    still trying to learn leaving a branch unchanged

---
+++ src/learning006/leavingTheBranchUnchanged.java
@@ -0,0 +1,9 @@
+package learning006;
+
+public class leavingTheBranchUnchanged {
+
+    public static void main(String[] args) {
+
+        System.out.println("I will leave this branch unchanged on GitHub");
+    }
+}
