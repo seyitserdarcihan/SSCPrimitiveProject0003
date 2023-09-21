4bde0cd6144c18827e19c312a090d3b5f6c1e58b
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-09-21 16:37:56 +0300

    bakalım bir dalı ana dala zorlamadan kendi halinde güncelleyip götürebiliyor muyuz

--- src/learning006/leavingTheBranchUnchanged.java
+++ src/learning006/leavingTheBranchUnchanged.java
@@ -5,5 +5,6 @@ public class leavingTheBranchUnchanged {
     public static void main(String[] args) {
 
         System.out.println("I will leave this branch unchanged on GitHub");
+        System.out.println("Bakalım bir dalı kendi halinde GitHub'da güncellemeye devam edebiliyor muyuz.");
     }
 }
